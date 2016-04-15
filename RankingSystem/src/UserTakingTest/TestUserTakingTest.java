731a1fc1feb62436cfe6a39edac25e9feb9c2a89
Ian Burton <mudtar@gmail.com>
2016-04-15 03:55:26 -0700

    Created a skeleton for the JavaFX application

--- RankingSystem/src/UserTakingTest/TestUserTakingTest.java
+++
@@ -1,8 +0,0 @@
-package UserTakingTest;
-
-/**
- * @author BeeYean Tan
- * @version 2016-04-11
- */
-public class TestUserTakingTest {
-}
---
+++ RankingSystem/src/usertest/src/Main.java
@@ -0,0 +1,39 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * This class serves as an entry point for the user test component of
+ * the hypnotherapy preference ranking system.
+ *
+ * @author  Ian Burton
+ * @version 2016.04.15.01
+ */
+public class Main extends Application {
+    /**
+     * The JavaFX entry point.
+     *
+     * @param  primaryStage The stage created by JavaFX for use with
+     *                      this application.
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("user_test.fxml"));
+        primaryStage.setTitle("Select Your Preference");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    /**
+     * The entry point to the class which launches the JavaFX
+     * application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
