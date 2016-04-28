6df9e2e68664c7c09c2ab9d4629a5104f29f94a3
David L <lee.david.usa@gmail.com>
2016-04-28 12:25:05 -0700

    Clean code

--- src/main/java/edu/pcc/fbj/rankingsystem/dashboard/AdminDashboard.java
+++ src/main/java/edu/pcc/fbj/rankingsystem/dashboard/AdminDashboard.java
@@ -11,32 +11,28 @@ import java.awt.event.ActionListener;
 /**
  * AdminDashboard is used to test launching report panel.
  *
- * @Author: David Li
- * @Version: 2016.04.20
+ * @author David Li
+ * @version 2016.04.20
  * 1. Initial version
  *
- * @Author: David Li
- * @Version: 2016.04.25
+ * @author David Li
+ * @version 2016.04.25
  * 1. Launch report panel in another window.
  */
 public class AdminDashboard extends JPanel implements ActionListener {
 
-    private JButton reportBtn;
-    private JButton setupBtn;
     private static final String REPORT = "report";
     private static final String SETUP = "setup";
-    //private ReportTable data;
-
 
     public AdminDashboard(){
         setOpaque(true);
         setLayout(new GridBagLayout());
-        reportBtn = new JButton("Open Report List");
+        JButton reportBtn = new JButton("Open Report List");
         reportBtn.setActionCommand("report");
         reportBtn.addActionListener(this);
         add(reportBtn);
 
-        setupBtn = new JButton("Setup Test Items");
+        JButton setupBtn = new JButton("Setup Test Items");
         setupBtn.setActionCommand("setup");
         setupBtn.addActionListener(this);
 
@@ -49,32 +45,20 @@ public class AdminDashboard extends JPanel implements ActionListener {
      * @param e  - action event
      */
     public void actionPerformed(ActionEvent e){
+
+        JFrame frame = new JFrame();
+        frame.setPreferredSize(new Dimension(480,320));
         if(e.getActionCommand().equals(REPORT)) {
-            ReportTable data;
-            data = new ReportTable();
-            /*JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
-            JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
-            frame.setContentPane(data.getReportPanel());
-            frame.revalidate();
-            */
-            JFrame frame = new JFrame("User Test Result Report");
-            frame.setPreferredSize(new Dimension(500,300));
-            frame.setContentPane(data.getReportPanel());
-            frame.setLocationByPlatform(true);
-            frame.pack();
-            frame.setVisible(true);
+            ReportTable report = new ReportTable();
+            frame.setContentPane(report.getReportPanel());
+            frame.setTitle("Test Report");
+
         } else if(e.getActionCommand().equals(SETUP)){
             try
             {
-                JFrame frame = new JFrame("Admin Setup");
                 AdminSetupController adminSetupController = new AdminSetupController(frame);
-
-                frame.setPreferredSize(new Dimension(480,320));
-
                 frame.setContentPane(adminSetupController.getRootPanel());
-                frame.setLocationByPlatform(true);
-                frame.pack();
-                frame.setVisible(true);
+                frame.setTitle("Admin Setup");
             }
             catch (Exception ex)
             {
@@ -82,6 +66,9 @@ public class AdminDashboard extends JPanel implements ActionListener {
             }
 
         }
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setVisible(true);
 
     }
 }
