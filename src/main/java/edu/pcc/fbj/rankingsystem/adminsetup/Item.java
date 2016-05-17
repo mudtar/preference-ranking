637d8f1e868947b8b6e4ad09ac55958d8a611c73
Eric Kristiansen <eric.kristiansen@mail.com>
2016-05-17 13:53:53 -0700

    Sprint2 Gui Form work

--- src/main/java/edu/pcc/fbj/rankingsystem/adminsetup/Item.java
+++ src/main/java/edu/pcc/fbj/rankingsystem/adminsetup/Item.java
@@ -1,9 +1,11 @@
 package edu.pcc.fbj.rankingsystem.adminsetup;
 
+import javax.swing.*;
+
 /**
  * @author Eric Kristiansen
  *
- * version 5/3/16
+ * version 5/17/16
  *
  * This class was designed with future development in mind. It only holds a string variable
  * currently, but will eventually hold more
@@ -11,14 +13,16 @@ package edu.pcc.fbj.rankingsystem.adminsetup;
 public class Item
 {
     private String name;
+    //private Icon icon;
 
     /**
      * constructor for the Item object
      * @param passName
      */
-    public Item(String passName)
+    public Item(String passName /*, Icon passIcon */)
     {
         name = passName;
+        //icon = passIcon;
     }
 
     /**
@@ -34,8 +38,16 @@ public class Item
      * Method to return name independent of other variables of item object
      * @return returns name
      */
-    public String getName()
+    public String getName() { return name; }
+
+    /*
+    /**
+     * Method to return icon independent of other variables of item object
+     * @return icon
+     * /
+    public Icon getIcon()
     {
-        return name;
+        return icon;
     }
+    */
 }
